e5efa76ee64cda12815ada341c1ae48de9530c17
rhysr <rhysr@mega-city-one.co.uk>
2012-11-04 19:20:52 +0000

    catered for non-continuous numbers

--- day2/01-4numbers.rb
+++ day2/01-4numbers.rb
@@ -4,4 +4,14 @@
 puts
 
 #using each_with_index solves the problem of offset, but the problem only allows for each()
-(3..18).each_with_index { |i, idx| print "#{i} " ; puts if i % 4 == (i - idx - 1) % 4}
+(3..18).each_with_index { |i, idx| print "#{i} " ; puts if (idx + 1) % 4 == 0}
+
+puts
+
+#looking closer at the question, it doesn't state 16 consecutive numbers
+idx = 0
+[1,2,3,3,3,4,5,7,8,10,12,12,13,14,15,15].each do |i|
+    print i, ' '
+    puts if (idx + 1) % 4 == 0
+    idx += 1
+end
