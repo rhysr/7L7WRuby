fda3a87542422b78477b2be3cc3f841140078a47
rhysr <rhysr@mega-city-one.co.uk>
2012-11-04 19:05:58 +0000

    extra bit of first problem to handle offset on range

--- day2/01-4numbers.rb
+++ day2/01-4numbers.rb
@@ -1,2 +1,7 @@
 #works ok for this range, but if lower bound is not at 4n + 1 it won't work
 (1..16).each {|i| print "#{i} " ; puts if i % 4 == 0}
+
+puts
+
+#using each_with_index solves the problem of offset, but the problem only allows for each()
+(3..18).each_with_index { |i, idx| print "#{i} " ; puts if i % 4 == (i - idx - 1) % 4}
